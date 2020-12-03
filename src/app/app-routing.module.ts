47f3f574cbfabbc3d5c41f3199b9c42287323f03
MarcusVinicius <marcus.jose@etec.sp.gov.br>
2020-12-02 23:18:22 -0300

    Trabalho Interdisciplinar

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'categorias/listar',
+    loadChildren: () => import('./categorias/listar/listar.module').then( m => m.ListarPageModule)
+  },
+  {
+    path: 'categorias/cadastrar',
+    loadChildren: () => import('./categorias/cadastrar/cadastrar.module').then( m => m.CadastrarPageModule)
+  },
+  {
+    path: 'categorias/alterar/:id',
+    loadChildren: () => import('./categorias/alterar/alterar.module').then( m => m.AlterarPageModule)
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
